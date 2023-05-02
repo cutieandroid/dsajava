fb3cbce45b8739009a0f462109910096fafa0c8c
cutieandroid <20102161.harshmali@gmail.com>
2023-05-02 20:03:14 +0530

    creating a linked list, adding nodes and printing.

--- LInkedlist/Linkedlist.java
+++ LInkedlist/Linkedlist.java
@@ -26,7 +26,7 @@ public class Linkedlist {
 
    }
 
-   private void printlist(){ //function to print the linked list 
+   private void printList(){ //function to print the linked list 
     listnode current= ghead;
     
     while(current !=null){ //print the data of the linked list while we reach null. linked list's last node points to null
@@ -60,7 +60,7 @@ public class Linkedlist {
 
     //printting the linked list
 
-    ll.printlist();
+    ll.printList();
     
 
    }
