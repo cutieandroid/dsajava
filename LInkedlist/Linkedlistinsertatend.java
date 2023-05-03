fca07701c5e9d75df018ee751ad58c5a908212b4
cutieandroid <20102161.harshmali@gmail.com>
2023-05-03 22:26:54 +0530

    inserting a node at the end of the list

---
+++ LInkedlist/Linkedlistinsertatend.java
@@ -0,0 +1,69 @@
+package LInkedlist;
+
+public class Linkedlistinsertatend {
+    private listnode ghead; // head of the linked list
+    private listnode gend; // tail of the linked list
+
+    public static class listnode { // a static class for nodes of the linked list
+
+        // a node consist of two components := head and the pointer to next node
+
+        private int data;
+        private listnode next; // pointer to next node
+
+        private listnode(int data) { // a constructor to assign data and next node as soon as the node object is
+                                     // create
+
+            this.data = data;
+            this.next = next;
+
+        }
+
+    }
+
+    private void printList() { // function to print the linked list
+        listnode current = ghead;
+
+        while (current != null) { // print the data of the linked list while we reach null. linked list's last
+                                  // node points to null
+            System.out.print(current.data + "---->");
+            current = current.next;
+        }
+        System.out.println("null");
+
+    }
+
+    private void insertend(int data) {
+        listnode newnode = new listnode(data); // creating a new node
+        gend.next = newnode; // pointing the next of the current last node to the new node
+        gend = newnode; // assigning the new node as the last node
+    }
+
+    public static void main(String[] args) {
+
+        // creating an object of the Linkedlist class
+        Linkedlistinsertatend ll = new Linkedlistinsertatend();
+
+        // creating nodes using listnode class
+        listnode first = new listnode(10);
+        listnode second = new listnode(20);
+        listnode third = new listnode(30);
+        listnode fourth = new listnode(40);
+
+        // connecting all the nodes
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        ll.ghead = first;
+        ll.gend = fourth;
+
+        // printting the linked list
+
+        ll.printList();
+        ll.insertend(100);
+        System.out.println("list after inserting a node at the end:");
+        ll.printList();
+
+    }
+}
